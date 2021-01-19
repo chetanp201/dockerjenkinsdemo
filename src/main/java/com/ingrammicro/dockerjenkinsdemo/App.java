4f6e122f02f77584877fd417a848eac7a8036352
chetan patil <chetanp201@gmail.com>
2021-01-18 21:28:27 -0800

    first commit

---
+++ src/main/java/com/ingrammicro/dockerjenkinsdemo/App.java
@@ -0,0 +1,13 @@
+package com.ingrammicro.dockerjenkinsdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
